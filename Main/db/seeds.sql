750d2c5d3723f5ce6dca834634d769a504679791
peter kim <peterkim1025@gmail.com>
2023-11-19 13:10:09 -0500

    Seed troubleshoot update

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -15,7 +15,7 @@ INSERT INTO role (id, title, salary, department_id) VALUES
 
 -- Inserting sample data into 'employee' table
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-    (1, 'John', 'Doe', 1, 2),
-    (2, 'Jane', 'Smith', 2, 1),
-    (3, 'Bob', 'Johnson', 3, 1),
-    (4, 'Alice', 'Williams', 4, 2);
+    (1, 'Peter', 'Kim', 2, 1),
+    (2, 'Dasom', 'Lee', 1, 2),
+    (3, 'Ethan', 'Clelland', 3, 1),
+    (4, 'Conner', 'Hounshell', 4, 2);
